ff6709b9a5a2242d9fe9877a783fcf0bc08cd87f
sergey20x25 <vdoond@mail.ru>
2019-09-09 22:42:31 +0700

    fix ChannelInfo.jsx

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ChannelInfo = (props) => (
+const ChannelInfo = () => (
   <div className="navbar navbar-expand-lg p-3">
     <h6>Current channel name</h6>
   </div>
