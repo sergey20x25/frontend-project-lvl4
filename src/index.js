7f20aebf5110efdc8919d2ef84688d4fc6e3e386
sergey20x25 <vdoond@mail.ru>
2020-01-16 14:17:27 +0700

    changed server framework to fastify

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import '../assets/application.scss';
+
+// import faker from 'faker';
+// import gon from 'gon';
+// import cookies from 'js-cookie';
+// import io from 'socket.io-client';
+
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.debug = 'chat:*';
+}
